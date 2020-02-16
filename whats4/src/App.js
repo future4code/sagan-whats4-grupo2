8a3b66abf233dac37965dc852eaff8d762916455
EduCosta85 <59846713+EduCosta85@users.noreply.github.com>
2020-02-16 14:48:32 -0300

    deletar msg, cores unicas por user, digitando

--- whats4/src/App.js
+++ whats4/src/App.js
@@ -9,11 +9,100 @@ class App extends React.Component {
     super(props)
       this.state = { 
         arrayDeMsg: [
-          {autor: "Eu", msg: "Oi!"},
-          {autor: "Voce", msg: "Oi! Tudo bem?"},
-          {autor: "Voce", msg: "Esta melhor?"},
-          {autor: "Eu", msg: "Estou ótimo!"},
-        ]
+          {autor: "Eu", msg: "Oi!", color: "rgb(220, 248, 198)"},
+          {autor: "Voce", msg: "Oi! Tudo bem?", color: "Turquoise"},
+          {autor: "Voce", msg: "Esta melhor?", color: "Turquoise"},
+          {autor: "Eu", msg: "Estou ótimo!",  color: "rgb(220, 248, 198)"},
+        ],
+        arrayDeUsers: [
+          {usuario: "Eu", color: "rgb(220, 248, 198)"},
+          {usuario: "Voce", color: "Turquoise"},
+        ],
+        arrayDeCores: ["Turquoise",
+"MediumAquamarine",
+"RosyBrown",
+"Wheat",
+"Orchid",
+"Plum",
+"LightPink",
+"LightCoral",
+"Salmon",
+"Khaki",
+"AliceBlue",
+"GhostWhite",
+"Snow",
+"Seashell",
+"FloralWhite",
+"WhiteSmoke",
+"Beige",
+"OldLace",
+"Ivory",
+"Linen",
+"Cornsilk",
+"AntiqueWhite",
+"BlanchedAlmond",
+"Bisque",
+"LightYellow",
+"LemonChiffon",
+"LightGoldenrodYellow",
+"PapayaWhip",
+"PeachPuff",
+"Moccasin",
+"PaleGoldenrod",
+"MistyRose",
+"LavenderBlush",
+"Lavender",
+"Thistle",
+"Azure",
+"LightCyan",
+"PowderBlue",
+"PaleTurquoise",
+"Honeydew",
+"MintCream",
+"LightSkyBlue",
+"LightBlue",
+"LightSteelBlue",
+"Turquoise",
+"MediumAquamarine",
+"DarkSeaGreen",
+"YellowGreen",
+"RosyBrown",
+"Wheat",
+"Orchid",
+"Plum",
+"LightPink",
+"LightCoral",
+"Salmon",
+"Khaki",
+"AliceBlue",
+"GhostWhite",
+"Snow",
+"Seashell",
+"FloralWhite",
+"WhiteSmoke",
+"Beige",
+"OldLace",
+"AntiqueWhite",
+"BlanchedAlmond",
+"Bisque",
+"LightYellow",
+"LemonChiffon",
+"LightGoldenrodYellow",
+"PapayaWhip",
+"PeachPuff",
+"Moccasin",
+"PaleGoldenrod",
+"MistyRose",
+"LavenderBlush",
+"Lavender",
+"Thistle",
+"Azure",
+"LightCyan",
+"PowderBlue",
+"PaleTurquoise",
+"Honeydew",
+"MintCream"],
+mostraDigitando: false,
       }
   }
 
@@ -22,17 +111,67 @@ class App extends React.Component {
       this.atualizaArrayDeMsg()
     }
   }
+  aoClicar2x = (event) => {
+    const arrayDeMsgCopia = [...this.state.arrayDeMsg]
+   let removed = arrayDeMsgCopia.splice(event.target.id, 1);
+
+   this.setState({
+    arrayDeMsg: arrayDeMsgCopia
+  })
+  }
+
+  digitandoSim = () => {
+    this.setState({
+      mostraDigitando: true
+    })
+    setTimeout(() => {  this.setState({
+      mostraDigitando: false
+    }) }, 5000);
+
+  }
+  digitandoNao= () => {
+    this.setState({
+      mostraDigitando: false
+    })
+  }
+
+  aparecerDigitando = (mostra) => {
+    if (mostra) {
+      return (
+        <div id="digit">
+          <p> digitando... </p>
+        </div>
+      )
+  }
+  }
 
   atualizaArrayDeMsg = () => {
     const arrayDeMsgCopia = [...this.state.arrayDeMsg]
+    const arrayDeUsersCopia = [...this.state.arrayDeUsers]
     const novaMsgAutor = document.getElementById('autor').value
-    const novaMsgMensagem = document.getElementById('msg').value
-
-    arrayDeMsgCopia.push({msg: novaMsgMensagem, autor: novaMsgAutor})
+    let novaMsgMensagem = document.getElementById('msg').value
+    let novaCor = ""
+    let arrayUsersTemp = arrayDeUsersCopia.filter((i, index, array) => { return (i.usuario == document.getElementById('autor').value)})
+if ((document.getElementById('autor').value != "") && (document.getElementById('msg').value != "")) {
+  if (arrayUsersTemp.length >= 1) {
+    novaCor = arrayUsersTemp[0].color
+    } else {
+      novaCor = this.state.arrayDeCores[arrayDeUsersCopia.length]
+      arrayDeUsersCopia.push({usuario: document.getElementById('autor').value, color: novaCor})
+      console.log(novaCor)
+    arrayDeMsgCopia.push({msg: novaMsgMensagem, autor: novaMsgAutor, color: novaCor})
 
     this.setState({
-      arrayDeMsg: arrayDeMsgCopia
+      arrayDeMsg: arrayDeMsgCopia,
+      arrayDeUsers: arrayDeUsersCopia,
+      mostraDigitando: false
     })
+    document.getElementById('msg').value = ""
+    }
+} else {
+alert("Você precisa digitar o Usuario e a mensagem")
+}
+
   }
 
   render() {
@@ -40,21 +179,30 @@ class App extends React.Component {
     <div className="App">
       <div className="container">
         <div className="mensagem">
+        {this.aparecerDigitando(this.state.mostraDigitando)}
           {this.state.arrayDeMsg.map(elemento => {
             if ((elemento.autor === 'eu') || (elemento.autor === 'Eu') || (elemento.autor === 'EU')) {
               return (
-              <MensagemEu 
+              <div className="mensagemx" onDoubleClick={this.aoClicar2x} id={this.state.arrayDeMsg.indexOf(elemento)}>
+                <MensagemEu 
                 key={this.state.arrayDeMsg.indexOf(elemento)}
                 msg={elemento.msg}
+                color={elemento.color}
+                id={this.state.arrayDeMsg.indexOf(elemento)}
                 />
+              </div>
               )
             } else {
               return (
-              <MensagemTodos 
+              <div className="mensagemx" onDoubleClick={this.aoClicar2x} id={this.state.arrayDeMsg.indexOf(elemento)}>
+                <MensagemTodos 
                   key={this.state.arrayDeMsg.indexOf(elemento)}
                   autor={elemento.autor}
                   msg={elemento.msg}
+                  color={elemento.color}
+                  id={this.state.arrayDeMsg.indexOf(elemento)}
                 />
+              </div>
               )
               }
             }
@@ -63,11 +211,10 @@ class App extends React.Component {
         </div>
         <div className="teclado">
           <input type="text" placeholder="Usuário" id="autor"></input>
-          <input type="text" placeholder="Mensagem" id="msg" onKeyDown={this.aoApertarNoTeclado}></input>
+          <input type="text" placeholder="Mensagem" id="msg" onFocus={this.digitandoSim} onKeyDown={this.aoApertarNoTeclado}></input>
           <input type="submit" id="botaoEnviar" onClick={this.atualizaArrayDeMsg} ></input>
         </div>
       </div>
-
       </div>
   )}
 }
--- whats4/src/Components/MensagemEu/MensagemEu.js
+++ whats4/src/Components/MensagemEu/MensagemEu.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './MensagemEu.css';
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const FundoDinamico = styled.div`
+background-color: ${props => props.color};
+max-width: 40%;
+align-self: flex-end;
+display: flex;
+flex-direction: column;
+box-shadow: rgba(0, 0, 0, 0.13) 0px 1px 0.5px;
+padding: 10px 15px;
+border-radius: 5px;
+margin: 10px;
+`
 
 class MensagemEu extends React.Component {
   constructor(props) {
@@ -8,15 +21,14 @@ class MensagemEu extends React.Component {
     
   }
 
-
-  render() {
-   return(
-    <div className="mensagem-eu">
-      <div className="Mensagem-eu-mensagem">
-        {this.props.msg}
-      </div>
-    </div>
-  )
+render() {
+  return(
+     <FundoDinamico color={this.props.color}>
+   <div className="Mensagem-eu-mensagem" id={this.props.id}>
+     {this.props.msg}
+   </div>
+     </FundoDinamico>
+ )
 }
 
 }
